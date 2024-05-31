134046f913ad585e17cee571bd701a8e0293e699
Ihor Melnyk <73445190+Mentos302@users.noreply.github.com>
2024-05-31 15:41:20 +0300

    update meta

--- bc.php
+++ bc.php
@@ -13,12 +13,11 @@
  * @package          Bonus_Calculator
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/bc-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Plugin Name:       Bonus Calculator
+ * Description:       The Bonus Calculator plugin provides a customizable solution for betting websites, allowing administrators to create and manage various bonuses via the WordPress admin panel, with dynamic shortcode embedding for user-friendly access.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Ihor M.
+ * Author URI:        http://ihor.co
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       plugin-name
